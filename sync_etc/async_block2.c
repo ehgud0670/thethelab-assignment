341a72010f34cb229d26a1b5a02f5fcfb1550255
ehgud0670 <kimdo2297@gmail.com>
2019-05-04 19:02:01 +0900

    2019/05/04

--- sync_etc/async_block2.c
+++
@@ -1,6 +0,0 @@
-void check(int signum){
-   int status;
-   while(wait(&status)>0){
-     printf("status: %d \n " ,status);
-	 }
-}
--- sync_etc/async_nonblock.c
+++
@@ -1,7 +0,0 @@
-void check(int signum){
-  int signum;
-  while(waitpid(0,&status, WNOHANG) >0){
-    printf("status: %d \n" ,status);
-	}
- 
-}
